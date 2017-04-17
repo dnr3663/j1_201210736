cd5b7262d6f5b824ec52f3ffe70dd07f57859ee9
태욱 j1_201210736 <dnr3663@naver.com>
2017-04-17 22:30:31 +0900

    Add files via upload

---
+++ src/headfirst/factory/pizzas/PotatoPizza.java
@@ -0,0 +1,11 @@
+package headfirst.factory.pizzas;
+
+public class PotatoPizza extends Pizza {
+ public PotatoPizza() {
+  name = "Potato Pizza";
+  dough = "I don't know Crust";
+  sauce = "Tomato Pizza Sauce";
+  toppings.add("Fresh Karmamberall");
+  toppings.add("MushRoom 23");
+ }
+}
